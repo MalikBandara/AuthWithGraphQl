4f4cf1f52e2b2b29cbb021538b57f15d0a374348
MalikBandara <mmalith520@gmail.com>
2025-08-17 15:41:08 +0530

    change edit button

--- src/App.jsx
+++ src/App.jsx
@@ -128,7 +128,7 @@ function App() {
                 {user?.username === post.owner && (
                   <button
                     onClick={() => editPost(post)}
-                    className="px-4 py-2 bg-gradient-to-r from-emerald-400 to-cyan-400 text-white rounded-full text-sm font-medium hover:from-emerald-500 hover:to-cyan-500 transform hover:scale-105 transition-all duration-200 shadow-md opacity-0 group-hover:opacity-100"
+                    className="px-4 py-2 bg-gradient-to-r from-emerald-400 to-cyan-400  transition-all duration-200 shadow-md rounded  "
                   >
                     Edit
                   </button>
